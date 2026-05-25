bf0e6cbcecdbc5918131ff54999907a8f89963dd
harshith <harshith.ashvi@gmail.com>
2026-05-25 12:44:24 +0530

    Add cirlce and spring animation

--- AnimateExp/ContentView.swift
+++ AnimateExp/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var scaleValue: Double = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 200, height: 200)
+                .scaleEffect(scaleValue)
+                .animation(.spring(response: 0.3, dampingFraction: 0.5), value: scaleValue)
+                .onTapGesture {
+                    springBounce()
+                }
         }
         .padding()
+    }
+    private func springBounce() {
+        scaleValue = 0.6
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            scaleValue = 1
+        }
     }
 }
 
